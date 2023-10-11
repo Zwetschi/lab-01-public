63188f68ce790b2d99c54db6bacf46da9b77e91e
Philipp Gogolin <gogolin@hm.edu>
2023-10-11 18:10:20 +0200

    Add Comments to pub

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -16,15 +16,22 @@ class MyPublisherNode(DTROS):
         self._vehicle_name = os.environ["VEHICLE_NAME"]
         # construct publisher
         topic_name = "chatter"
-        self.pub = rospy.Publisher(topic_name, String, queue_size=10)
+        # queue size ist wie in queu modul in pyhon, wenn voll dann alte nachrichten raus LIFI FIFO
+        self.pub = rospy.Publisher(topic_name, String, queue_size=5)
 
     def run(self):
         # publish message every 1 second (1 Hz)
         rate = rospy.Rate(1)
         message = f"Hello from {self._vehicle_name}!"
         while not rospy.is_shutdown():
-            rospy.loginfo("Publishing message: '%s'" % message)
-            self.pub.publish(message)
+            rospy.loginfo("Publishing log message: '%s'" % message)
+            # implizit vom typ string publishen
+            self.pub.publish(message)  # sync
+            # synchronous FRAGE: wenn n sub abkratz dann führt das allgemein zu fehlern auch wenns nur ein bei einem subscriber nicht klappt?
+            # pub.publish(std_msgs.msg.String("hello world")) explizit
+            # std_msgs.msg.String hat n attribut data so:  pub.publish(data="hello world") is ok too
+            # std_msgs.msg.ColorRGBA has four fields (r, g, b, a), so we could call:
+            # pub.publish(255.0, 255.0, 255.0, 128.0)
             rate.sleep()
 
 
